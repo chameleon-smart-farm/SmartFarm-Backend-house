673cdf28c95c8c3cd1a96e92ec06bfea85d07edd
BakHyegyeong <calla3494qhk@naver.com>
2025-03-29 14:07:55 +0900

    fix : 기상청 데이터 저장, 가져오기 메서드 버그 수정

--- src/main/java/com/smartfram/chameleon_house/domain/weather/dao/WeatherMapper.java
+++ src/main/java/com/smartfram/chameleon_house/domain/weather/dao/WeatherMapper.java
@@ -3,6 +3,7 @@ package com.smartfram.chameleon_house.domain.weather.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.smartfram.chameleon_house.domain.weather.dto.WeatherDataDTO;
 
@@ -13,6 +14,6 @@ public interface WeatherMapper {
     public void create_weather_data(List<WeatherDataDTO> weatherDataList);
 
     // DB의 기상청 데이터 가져오기
-    public WeatherDataDTO read_weather_data(String cur_date, String cur_time);
+    public WeatherDataDTO read_weather_data(@Param("cur_date") String cur_date, @Param("cur_time") String cur_time);
     
 }
--- src/main/java/com/smartfram/chameleon_house/global/scheduler/Scheduler.java
+++ src/main/java/com/smartfram/chameleon_house/global/scheduler/Scheduler.java
@@ -20,14 +20,14 @@ public class Scheduler {
 
     // 초(0-59) 분(0-59) 시간(0-23) 일(1-31) 월(1-12) 요일(0-6)
     @Scheduled(cron = "0 30 5 */3 * *")
-    // @Scheduled(cron = "*/10 * * * * *") - test용
+    // @Scheduled(cron = "* */1 * * * *")
     public void getWeatherData() {
 
         // 현재 시각
         LocalDateTime now = LocalDateTime.now();
 
         // yynndd, hhmm 형식으로 변환
-        String cur_date = now.format(DateTimeFormatter.ofPattern("yyMMdd"));
+        String cur_date = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
         // String cur_time = now.format(DateTimeFormatter.ofPattern("HHmm"));
 
         weatherService.save_weather_info(cur_date);
