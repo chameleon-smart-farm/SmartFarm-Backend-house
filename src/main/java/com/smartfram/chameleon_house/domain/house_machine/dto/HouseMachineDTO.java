70bdc82e3d7692be3731cd9fffc579cff92c789a
BakHyegyeong <calla3494qhk@naver.com>
2025-04-08 23:38:54 +0900

    feat : 모터 상태 조회, 모터 on/off API 구현

---
+++ src/main/java/com/smartfram/chameleon_house/domain/house_machine/dto/HouseMachineDTO.java
@@ -0,0 +1,11 @@
+package com.smartfram.chameleon_house.domain.house_machine.dto;
+
+import lombok.Data;
+
+@Data
+public class HouseMachineDTO {
+
+    // 모터 상태
+    private boolean motor_status;
+    
+}
